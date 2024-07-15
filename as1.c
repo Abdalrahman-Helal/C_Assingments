c918208d4d1627968a10f46bb680d75d6ed9dd40
Abdalrahman Helal <121490216+Abdalrahman-Helal@users.noreply.github.com>
2024-07-15 09:04:30 +0300

    Add files via upload
    Write a C code that defines a function which takes an array as input argument and
    apply the bubble sorting algorithm on it, then print the result

--- as1.c
+++ as1.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
 int main(void)
 {
-    int tr=1 , fls=0;
-    printf("True in GCC is : %d\n",tr);
-    printf("false in GCC is : %d",fls);
+    int ID;
+    int num1,num2;
+    printf("Please Enter Operation ID");
+    scanf("%d",&ID);
+   switch(ID)
+   {
+    case 1:
+    printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1+num2);
+    break;
 
+    case 2:
+    printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1-num2);
+    break;
+
+    case 3:
+    printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1*num2);
+    break;
+
+    case 4:
+    printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1/num2);
+    break;
+
+    case 5:
+    printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1&num2);
+    break;
+
+    case 6:
+    printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1|num2);
+    break;
+
+    case 7:
+    printf("Please Enter 1 Operand");
+    scanf("%d",&num1);
+    printf("%d",~num1);
+    break;
+
+    case 8:
+    printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1^num2);
+    break;
+    case 9:
+     printf("Please Enter 2 Operand");
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1%num2);
+    break;
+
+    case 10:
+    printf("Please Enter 1 Operand");
+    scanf("%d",&num1);
+    printf("%d",++num1);
+    break;
+
+    case 11:
+    printf("Please Enter 1 Operand");
+    scanf("%d",&num1);
+    printf("%d",--num1);
+    break;
+   }
 }
